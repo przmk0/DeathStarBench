77e7064fa122329a9303336a00aaab2ec1ddd7af
Przemek Tyrkiel <przemyslaw.tyrkiel@intel.com>
2022-09-12 10:49:14 +0200

    Update read.lua

--- socialNetwork/nginx-web-server/lua-scripts/wrk2-api/home-timeline/read.lua
+++ socialNetwork/nginx-web-server/lua-scripts/wrk2-api/home-timeline/read.lua
@@ -59,7 +59,7 @@ function _M.ReadHomeTimeline()
   local tracer = bridge_tracer.new_from_global()
   local parent_span_context = tracer:binary_extract(
       ngx.var.opentracing_binary_context)
-  GenericObjectPool:setMaxTotal(256)
+  -- GenericObjectPool:setMaxTotal(256)
   local span = tracer:start_span("read_home_timeline_client",
       { ["references"] = { { "child_of", parent_span_context } } })
   local carrier = {}
@@ -76,15 +76,15 @@ function _M.ReadHomeTimeline()
   end
 
 
-  local get_client_span = tracer:start_span("read_home_timeline_get_client_from_pool",
-      { ["references"] = { { "child_of", parent_span_context } } })
-  local get_client_carrier = {}
-  tracer:text_map_inject(span:context(), carrier)
+--   local get_client_span = tracer:start_span("read_home_timeline_get_client_from_pool",
+--       { ["references"] = { { "child_of", parent_span_context } } })
+--   local get_client_carrier = {}
+--   tracer:text_map_inject(span:context(), carrier)
 
   local client = GenericObjectPool:connection(
       HomeTimelineServiceClient, "home-timeline-service" .. k8s_suffix, 9090)
 
-  get_client_span.finish()
+--  get_client_span.finish()
   
   local status, ret = pcall(client.ReadHomeTimeline, client, req_id,
       tonumber(args.user_id), tonumber(args.start), tonumber(args.stop), carrier)
